a8351d9798e73bb382a39573563c805108f551ca
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-11-26 18:24:34 +0300

    Fix props of svg

--- src/components/icons/PlusIcon.tsx
+++ src/components/icons/PlusIcon.tsx
@@ -5,9 +5,9 @@ export function PlusIcon({ className }: { className?: string }) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <circle cx="12" cy="12" r="10" /> <line x1="12" y1="8" x2="12" y2="16" />
       <line x1="8" y1="12" x2="16" y2="12" />
