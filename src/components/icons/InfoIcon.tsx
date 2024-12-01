40b3acae9399b45f68c54dac4cc918db0df6e29a
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-12-01 19:17:37 +0300

    Create InfoIcon and LoadingIcon

---
+++ src/components/icons/InfoIcon.tsx
@@ -0,0 +1,17 @@
+export function InfoIcon({ className }: { className?: string }) {
+  return (
+    <svg
+      className={className}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      {' '}
+      <circle cx="12" cy="12" r="10" /> <line x1="12" y1="16" x2="12" y2="12" />{' '}
+      <line x1="12" y1="8" x2="12.01" y2="8" />
+    </svg>
+  );
+}
--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,1 +1,3 @@
 export { PlusIcon } from './PlusIcon';
+export { InfoIcon } from './InfoIcon';
+export { LoadingIcon } from './LoadingIcon';
