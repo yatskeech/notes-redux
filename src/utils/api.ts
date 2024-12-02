5ef7788070c922d0a3f0a5e3bf887e6cd1601ba3
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-12-03 00:36:18 +0300

    Add new function to working with DB

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,68 @@
-import { User } from '../types';
+import { Note, User } from '../types';
 import { data } from 'react-router';
 import { RegisterActionData } from './actions';
 
 const URL_API = 'http://localhost:3000';
 
 type UserParams = { [key in keyof User]?: string };
+type NoteParams = { [key in keyof Note]?: string };
+
+export async function putNote(note: Note) {
+  const response = await fetch(`${URL_API}/notes/${note.id}`, {
+    method: 'PUT',
+    body: JSON.stringify(note),
+  });
+
+  if (!response.ok) {
+    return;
+  }
+
+  return response.json();
+}
+
+export async function postNote(note: Note) {
+  const response = await fetch(`${URL_API}/notes/`, {
+    method: 'POST',
+    body: JSON.stringify(note),
+  });
+
+  if (!response.ok) {
+    return;
+  }
+
+  return response.json();
+}
+
+export async function getNoteById(id: string): Promise<Note | null> {
+  const response = await fetch(`${URL_API}/notes/${id}`);
+
+  if (!response.ok) {
+    return null;
+  }
+
+  return response.json();
+}
+
+export async function findNotesBy(noteParams: NoteParams): Promise<Note[]> {
+  const params = new URLSearchParams(noteParams);
+  const response = await fetch(`${URL_API}/notes?${params}`);
+
+  if (!response.ok) {
+    return [];
+  }
+
+  return response.json();
+}
+
+export async function getUserById(id: string): Promise<User | null> {
+  const response = await fetch(`${URL_API}/users/${id}`);
+
+  if (!response.ok) {
+    return null;
+  }
+
+  return response.json();
+}
 
 export async function findUsersBy(userParams: UserParams): Promise<User[]> {
   const params = new URLSearchParams(userParams);
