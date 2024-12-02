7f648e60129e3348efa2b2c1d1853a91851005d3
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-12-02 23:44:15 +0300

    Define interface Note

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,11 @@ export interface User {
   password: string;
   createdAt: number;
 }
+
+export interface Note {
+  id: string;
+  userId: string;
+  title: string;
+  content: string;
+  createdAt: number;
+}
