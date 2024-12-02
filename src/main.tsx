cc7dde062b0ec9fd1d640ea49f87b34d30a1adc6
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-12-03 00:37:00 +0300

    Wrap in UserContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { router } from './router';
 import './index.css';
+import { UserContextProvider } from './contexts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <UserContextProvider>
+      <RouterProvider router={router}/>
+    </UserContextProvider>
   </StrictMode>
 );
