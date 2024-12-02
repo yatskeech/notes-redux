db82025d9835db270c65da6c709f2987c44dd26d
Dmitry Yatskevich <dima.ja06@gmail.com>
2024-12-03 00:34:03 +0300

    Add new routes to router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,7 +1,15 @@
-import { createBrowserRouter } from 'react-router';
+import { createBrowserRouter, Navigate } from 'react-router';
 import { AuthLayout, Layout, PanelLayout } from '../layouts';
-import { LoginPage, ProfilePage, RegisterPage } from '../pages';
+import {
+  CreateNotePage,
+  LoginPage,
+  NotePage,
+  ProfilePage,
+  RegisterPage,
+} from '../pages';
 import { loginAction, registerAction } from '../utils/actions';
+import { RequireAuth } from '../utils/RequireAuth';
+import { NotesContextProvider } from '../contexts';
 
 export const router = createBrowserRouter([
   {
@@ -9,11 +17,29 @@ export const router = createBrowserRouter([
     element: <Layout />,
     children: [
       {
-        element: <PanelLayout />,
+        element: (
+          <RequireAuth>
+            <NotesContextProvider>
+              <PanelLayout />
+            </NotesContextProvider>
+          </RequireAuth>
+        ),
         children: [
+          {
+            index: true,
+            element: <CreateNotePage />,
+          },
+          {
+            path: ':id',
+            element: <NotePage />,
+          },
           {
             path: '/profile',
             element: <ProfilePage />,
+          },
+          {
+            path: '*',
+            element: <Navigate to="/" />,
           },
         ],
       },
